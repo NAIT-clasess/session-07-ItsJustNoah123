5bbd0b65181f096f9fef91a74e3a7b28994c2481
ItsJustNoah123 <noahhemmelgarn204@hotmail.com>
2026-01-30 10:54:38 -0700

    Draw and move rectangle using 1x1 texture
    Add position, dimensions, speed and a 1x1 pixel texture to render a rectangle and animate it across the screen. Initialize _position, _dimensions and _speed in Initialize, create the _pixel texture in LoadContent, draw the rectangle in Draw using SpriteBatch, and update its X position each frame in a new Move(GameTime) method called from Update. Useful for simple sprite/GUI prototyping or debug visuals.

--- Game1.cs
+++ Game1.cs
@@ -8,6 +8,10 @@ public class Game1 : Game
 {
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
+    private Vector2 _position;
+    private Vector2 _dimensions;
+    private float _speed;
+    private Texture2D _pixel;
 
     public Game1()
     {
@@ -19,6 +23,9 @@ public class Game1 : Game
     protected override void Initialize()
     {
         // TODO: Add your initialization logic here
+        _position = new Vector2(60f, 60f);
+        _dimensions = new Vector2(250f, 50f);
+        _speed = 120f;
 
         base.Initialize();
     }
@@ -28,6 +35,8 @@ public class Game1 : Game
         _spriteBatch = new SpriteBatch(GraphicsDevice);
 
         // TODO: use this.Content to load your game content here
+        _pixel = new Texture2D(GraphicsDevice, 1, 1);
+        _pixel.SetData(new[] { Color.White });
     }
 
     protected override void Update(GameTime gameTime)
@@ -36,6 +45,7 @@ public class Game1 : Game
             Exit();
 
         // TODO: Add your update logic here
+        Move(gameTime);
 
         base.Update(gameTime);
     }
@@ -43,9 +53,25 @@ public class Game1 : Game
     protected override void Draw(GameTime gameTime)
     {
         GraphicsDevice.Clear(Color.CornflowerBlue);
+        _spriteBatch.Begin();
+        Rectangle rect = new Rectangle(
+            (int)_position.X,
+            (int)_position.Y,
+            (int)_dimensions.X,
+            (int)_dimensions.Y
+        );
 
         // TODO: Add your drawing code here
+        _spriteBatch.Draw(_pixel, rect, Color.Black);
+        
+        _spriteBatch.End();
 
         base.Draw(gameTime);
+    }
+
+    private void Move(GameTime gameTime)
+    {
+        float seconds = (float)gameTime.ElapsedGameTime.TotalSeconds;
+        _position.X += _speed * seconds;
     }
 }
